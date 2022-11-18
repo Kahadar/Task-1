6f490a69aa7a5f0e11a5324ae19cc035a59b949e
Kahadar <solntsedar72@gmail.com>
2022-11-18 18:41:47 +0300

    выполнено

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,42 @@
 package jm.task.core.jdbc.util;
 
+//import com.sun.jdi.connect.spi.Connection;
+//import org.hibernate.hql.internal.ast.tree.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class Util {
-    // СЂРµР°Р»РёР·СѓР№С‚Рµ РЅР°СЃС‚СЂРѕР№РєСѓ СЃРѕРµРґРµРЅРµРЅРёСЏ СЃ Р‘Р”
+    public static final String USER_NAME = "postgres";
+    public static final String PASSWORD = "root";
+    public static final String URL = "jdbc:postgresql://localhost:5432/testTask";
+    private static final String DRIVER = "org.postgresql.Driver";
+    public static Statement statement;
+    public static Connection connection;
+
+
+    public Util(){};
+
+
+    public static Connection getConnection() {
+        try{
+            Class.forName(DRIVER);
+            connection =
+                    DriverManager.getConnection(URL,USER_NAME,PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+//    public static Connection getConnection() {
+//        return connection;
+//    }
+
+    public static void setConnection(Connection connection) {
+        Util.connection = connection;
+    }
+
+
+    // реализуйте настройку соеденения с БД
 }
